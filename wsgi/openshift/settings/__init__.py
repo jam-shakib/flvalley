2ee0e5f23984df979023ef5492872cb638620547
jamshid <jamshid@alaki.com>
2013-11-04 21:02:11 -0500

    account app activated for user login

--- wsgi/openshift/settings/__init__.py
+++ wsgi/openshift/settings/__init__.py
@@ -98,7 +98,7 @@ INSTALLED_APPS = (
     "mezzanine.pages",
     "mezzanine.galleries",
     "mezzanine.twitter",
-    #"mezzanine.accounts",
+    "mezzanine.accounts",
     #"mezzanine.mobile",
 	
 )
