34e7fa07abe295fe0a5939071fbc78e299558fd0
amoojamshid <jam.shakib@alaki.com>
2015-07-01 15:37:32 -0400

    changed database credentials

--- wsgi/openshift/settings/production.py
+++ wsgi/openshift/settings/production.py
@@ -8,9 +8,9 @@ ROOT_URLCONF = "%s.urls" % PROJECT_DIRNAME
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'bakery',
-        'USER': 'admingiugwah',
-        'PASSWORD': 'i9IResh6qHrS',
+        'NAME': 'python',
+        'USER': ' adminx8ms3ih',
+        'PASSWORD': 'WqLD1uKvFzAB',
         'HOST': '',
         'PORT': '',
     }
